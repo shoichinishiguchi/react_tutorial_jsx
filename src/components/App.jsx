0e5e4f2b6f7e6ab04d2f76193321f6a04cfe5598
shoichinishiguchi <48884623+shoichinishiguchi@users.noreply.github.com>
2021-10-09 14:03:26 +0900

    winnerの表示を洒落た感じに

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -67,7 +67,7 @@ const Borad = () => {
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
+        return `winner: ${squares[a]}`;
       }
     }
     return null;
