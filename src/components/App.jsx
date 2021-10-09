f0de602f4b8377447e30eae18b9ca3f952f90ae2
shoichinishiguchi <48884623+shoichinishiguchi@users.noreply.github.com>
2021-10-09 13:21:26 +0900

    feat: ばつ印がつくように

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,68 @@
-import { FC } from "react";
+import { useState } from 'react';
 import styled from "styled-components";
 
+
+const SSquare = styled.button`
+  display: block;
+  width: 64px;
+  height: 64px;
+  border: solid 1px #000;
+  font-size: 32px;
+  line-height: 64px;
+  text-align: center;
+  background-color: #fff;
+`;
+
+const SFlex = styled.div`
+display: flex;
+`;
+
+const Square = (props) => {
+  const { num, clickAction } = props;
+
+
+  return(
+    <>
+      <SSquare onClick={clickAction}>
+        {num}
+      </SSquare>
+    </>
+  )
+};
+
+const Borad = () => {
+  const [squares, setSquares] = useState(Array(9).fill(''));
+
+  const clickSquareAction = (i) => {
+    const newSquares = [...squares];
+    newSquares[i] = 'X';
+    setSquares(newSquares);
+  }
+
+  return(
+    <>
+      <div>
+        { [0,1,2].map((i) => (
+            (<SFlex key={i}>
+              <Square num = {squares[3*i + 0]} clickAction={() => clickSquareAction(3*i + 0)} />
+              <Square num = {squares[3*i + 1]} clickAction={() => clickSquareAction(3*i + 1)} />
+              <Square num = {squares[3*i + 2]} clickAction={() => clickSquareAction(3*i + 2)} />
+            </SFlex>)
+        ))
+        }
+      </div>
+    </>
+  )
+}
+
+
 export const App = () => {
+
 
   return(
     <div>
       <h1>丸罰ゲーム</h1>
+      <Borad/>
     </div>
   )
 
